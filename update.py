d7ce76ad8a83b3a8da2c8f3f38758e2796a1f0cf
Wojtek_Mac <wojciech.chamski@gmail.com>
2018-12-18 17:29:18 +0100

    Update
    create .docx instead of .txt (probably doesn't work)

--- update.py
+++ update.py
@@ -1,4 +1,7 @@
+# -*-coding: utf-8 -*-
+
 import pandas as pd
+from docx import Document
 import platform
 import datetime
 import subprocess as sp
@@ -10,14 +13,35 @@ from email.mime.text import MIMEText
 
 
 def sys_check():
-    if "darwin" in platform.system():
-        print("Mac OS X")
+    if "Darwin" in platform.system():
+        file_edit_mac()
     elif "Windows" in platform.system():
         file_edit_win()
     elif "linux" in platform.system():
-        print("linux")
+        file_edit_linux()
     else:
-        print(platform.system())
+        print(platform.system() + "nie jest wspieraną platformą.")
+
+
+def raport_doc():
+    document = Document()
+    document.add_heading("Raport z dnia "+ date, 0)
+    document.add_paragraph("Cześć!")
+    d = document.add_paragraph("Oto stan magazynowy z dnia " + date + ". ")
+    d.add_run("Na ten moment nie ma koniecznośći zamawiania dostawy, ")
+    d.add_run("ale musimy zamówić kuriera w celu odwesłania zwrotów.")
+    d.add_run(" ")
+    d.add_run("Pozdrawiam,")
+    table = document.add_table(rows=(df1.shape[0]), cols=df1.shape[1])  # First row are table headers!
+    for i, column in enumerate(df1) :
+        for row in range(df1.shape[0]) :
+            table.cell(row, i).text = str(df1[column][row])
+
+
+    document.add_page_break()
+
+    document.save('demo.docx')
+
 
 
 def clear_mail():
@@ -38,11 +62,20 @@ def file_edit_win():
 
 
 def file_edit_mac():
-    programName = "/Volumes/SSD/Applications/TextEdit.app"
-    textfile = "stan.txt"
-    p1 = sp.Popen([programName, textfile])
+    textfile = "demo.docx"
+    p = sp.Popen(["open", "-W", textfile])
+    p.wait()
 
-sys_check()
+
+
+def file_edit_linux():
+    textfile = "stan.txt"
+    p = sp.Popen(["gedit", textfile])
+    p.wait()
+
+
+#sys_check()
+
 
 now = datetime.datetime.now() - timedelta(1)
 yesterday = "9.11.2018"
@@ -54,7 +87,7 @@ df1 = df.loc[df["DATA"] == date]
 output = df1.to_dict(orient='list')
 table = tabulate(output, tablefmt = "grid", headers = "keys")
 
-
+#raport_doc()
 
 #clear_mail()
 
@@ -63,4 +96,4 @@ stan.write("\n")
 stan.write("\n")
 stan.write(table)
 stan.close()
-#df.to_csv(w'/Volumes/HDD/Python/stan.txt', df1)
+print(df1)
